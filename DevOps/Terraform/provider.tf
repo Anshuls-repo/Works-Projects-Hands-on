c0fd40f13eb017028d3e31f218c56bd8eb6c5cde
Anshul <121981323+Anshuls-repo@users.noreply.github.com>
2023-07-17 16:17:57 +0530

    Update provider.tf

--- DevOps/Terraform/provider.tf
+++ DevOps/Terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA6O72EWITGCAAFYKO"
-  secret_key = "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"
+  access_key = "yes"
+  secret_key = "yes"
 }
